ebe24580e580399fdfbff6128efac071b854433b
Avigyan-DEV <avigyanbasu2@gmail.com>
2025-12-06 02:24:57 +0530

    sixteenth commit

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,6 +1,6 @@
 export const BASE_URL =
   import.meta.env.VITE_BACKEND_URL ||
-  "https://ecom-store-mern-backend.onrender.com" ||
+  "https://ecom-store-mern-backend.onrender.com/api" ||
   "http://localhost:5000";
 
 export const USERS_URL = `${BASE_URL}/api/users`;
