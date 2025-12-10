4fb0480e1d746d419b295f50112d3339fb0886a4
Avigyan-DEV <avigyanbasu2@gmail.com>
2025-12-10 23:36:26 +0530

    product creation 4

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -1,27 +1,10 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constants";
-import { store } from "../redux/store"; // import your Redux store
 
-const baseQuery = fetchBaseQuery({
-  baseUrl: BASE_URL,
-  credentials: "include", // include cookies if your backend uses sessions
-  prepareHeaders: (headers) => {
-    // Get token from Redux store
-    const state = store.getState();
-    const token = state.auth.user?.token;
-
-    if (token) {
-      headers.set("Authorization", `Bearer ${token}`);
-    }
-
-    // Don't set Content-Type here, because FormData will automatically set it
-    return headers;
-  },
-});
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
 export const apiSlice = createApi({
-  reducerPath: "api",
   baseQuery,
   tagTypes: ["Product", "Order", "User", "Category"],
-  endpoints: () => ({}), // endpoints will be injected in other slices
+  endpoints: () => ({}),
 });
