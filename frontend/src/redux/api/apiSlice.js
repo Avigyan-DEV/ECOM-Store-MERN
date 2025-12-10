740d84b59a2d39e12982354be65e611c8324aa55
Avigyan-DEV <avigyanbasu2@gmail.com>
2025-12-10 22:20:12 +0530

    image url fix 19

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -5,7 +5,7 @@ export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL, // e.g., https://ecom-store-mern-backend.onrender.com
-    credentials: "include", // include cookies if your backend uses sessions
+    // credentials: "include", // include cookies if your backend uses sessions
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");
       return headers;
