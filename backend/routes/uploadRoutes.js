ec1a3db251b971deed518c7cb54d0e0f48b21ab5
Avigyan-DEV <avigyanbasu2@gmail.com>
2025-12-10 20:28:59 +0530

    image url fix 5

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,55 +1,35 @@
-import path from "path";
 import express from "express";
 import multer from "multer";
-import fs from "fs";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
 
 const router = express.Router();
 
-const uploadsDir = "uploads";
-
-if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    const extname = path.extname(file.originalname);
-    cb(null, `${file.fieldname}-${Date.now()}${extname}`);
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "ecommerce_uploads",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  const filetypes = /jpe?g|png|webp/;
-  const mimetypes = /image\/jpe?g|image\/png|image\/webp/;
-
-  const extname = path.extname(file.originalname).toLowerCase();
-  const mimetype = file.mimetype;
-
-  if (filetypes.test(extname) && mimetypes.test(mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new Error("Images only"), false);
-  }
-};
-
-const upload = multer({ storage, fileFilter });
+const upload = multer({ storage });
 const uploadSingleImage = upload.single("image");
 
 router.post("/", (req, res) => {
   uploadSingleImage(req, res, (err) => {
     if (err) {
-      res.status(400).send({ message: err.message });
-    } else if (req.file) {
-      res.status(200).send({
-        message: "Image Uploaded successfully",
-        image: `https://ecom-store-mern-backend.onrender.com/${req.file.path}`,
-      });
-    } else {
-      res.status(400).send({ message: "No image file provided" });
+      return res.status(400).send({ message: err.message });
     }
+    if (!req.file) {
+      return res.status(400).send({ message: "No image file provided" });
+    }
+
+    res.status(200).send({
+      message: "Image uploaded successfully",
+      imageUrl: req.file.path, // Cloudinary URL
+      publicId: req.file.filename,
+    });
   });
 });
 
