525388684e0c05709c2d229da6d01512fbdc5c51
Avigyan-DEV <avigyanbasu2@gmail.com>
2026-02-11 03:45:51 +0530

    Fix case-sensitive path for favorites slice

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-vercel-app-name.vercel.app",
+      "https://ecom-store-mern-beryl.vercel.app",
     ],
     credentials: true,
   })
